9f39c53d904e7f967f838a46ff7bfea6ccddf144
BaldGaming <itayrosen04@gmail.com>
2026-03-26 12:08:20 +0200

    updated some stuff

---
+++ lab2/ex1.sh
@@ -0,0 +1,23 @@
+clear
+
+echo "the script starts now!"
+
+echo "Hi, $USER!"
+
+echo
+
+echo "You can see the file's list of your directory now."
+
+ls -l
+
+echo
+
+echo "I'm going to delete your directory now."
+
+rm my
+
+echo
+
+echo "I'm giving you back your prompt now"
+
+echo
